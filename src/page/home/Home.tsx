70ae24e63ef28d6617f05afe316d577078a421ae
Andrew Newstead <andrew@doodlecube.co.uk>
2022-09-06 13:08:00 +0100

    returning code to resolve filenames

---
+++ src/page/home/Home.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import React from "react";
+import { Button, Card, Container, Grid } from "@mui/material";
+
+import AppLayout from "../../containers/app-layout";
+import useStyles from "./home.style";
+import type { ICheckbox, IMainData, IRootState } from "../../lib/types";
+import { NextLinkComposed } from "../../components/next-mui-link/Link";
+import { thumbHelper } from "../../lib/helpers";
+import { useAppSelector } from "../../lib/store";
+
+type Props = {
+  projects: IMainData[];
+};
+
+const Home = (props: Props) => {
+  const { projects } = props;
+  const { classes } = useStyles();
+
+  const baseContentURL = useAppSelector((state: IRootState) => {
+    return state.app.baseContentURL;
+  });
+  const checkboxes = useAppSelector((state: IRootState) => {
+    return state.app.nav.checkboxes;
+  }) as ICheckbox[];
+
+  const displayThumbs = thumbHelper(projects, checkboxes);
+
+  let content = <></>;
+
+  if (displayThumbs) {
+    if (displayThumbs.length) {
+      const thumbs = displayThumbs.map((obj) => {
+        const url = `${baseContentURL}${obj.thumb}`;
+        const alt = `${obj.client} - ${obj.brand} - ${obj.project}`;
+        return (
+          <Grid item key={obj.id} className={classes.gridItem}>
+            <Card elevation={6}>
+              <Button
+                component={NextLinkComposed}
+                to={`/project/${obj.id}`}
+                className={classes.gridItemButton}
+              >
+                <Image src={url} alt={alt} width={128} height={128} />
+              </Button>
+            </Card>
+          </Grid>
+        );
+      });
+      content = <>{thumbs}</>;
+    } else {
+      content = <Card className={classes.info}>Please make a selection</Card>;
+    }
+  }
+
+  return (
+    <AppLayout headerNavType="thumbs">
+      <Container className={classes.indexRoot}>
+        <Grid container spacing={2} justifyContent="center">
+          {content}
+        </Grid>
+      </Container>
+    </AppLayout>
+  );
+};
+
+export default Home;
